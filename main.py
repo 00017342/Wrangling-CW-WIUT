7af83b5f886651a1dafb7cc51f5a31a48e02830b
00018431 <oktamabdurazzokof@gmail.com>
2026-03-03 14:35:28 +0500

    Finished last two tabs, now we will think about other skeleton modifications and design too!

--- main.py
+++ main.py
@@ -153,8 +153,183 @@ with cleaningStudioTab:
 
 ##setting overview tab (will need change)
 with visualizationTab:
-    st.header("here will be visualization")
+    st.header("Visualization")
+    st.write("Create interactive charts and explore your dataset visually")
+    
+    chartConfigColumn, chartOutputColumn = st.columns([1,1])
+    
+    with chartConfigColumn:
+        containerVisualizationTab = st.container(border=True)
+        with containerVisualizationTab:
+            st.header("Chart Configuration")
+
+            histColumn, boxColumn = st.columns([2,2])
+
+            with histColumn:
+                st.button("Histogram")
+            
+            with boxColumn:
+                st.button("Box Plot")
+            
+            scatColumn, linecColumn = st.columns([2,2])
+
+            with scatColumn:
+                st.button("Scatter Plot")
+            
+            with linecColumn:
+                st.button("Line Chart")
+
+            gbarColumn, cheatColumn = st.columns([2,2])
+
+            with gbarColumn:
+                st.button("Grouped Bar Chart")
+            
+            with cheatColumn:
+                st.button("Correlation Heatmap")
+            
+            st.space(size=10)
+
+            st.header("Axes")
+
+            st.write("X-axis")
+            xaxis = st.popover("Choose")
+            xaxisopt1 = xaxis.checkbox("option xaxis 1")
+            xaxisopt2 = xaxis.checkbox("option xaxis 2")
+            xaxisopt3 = xaxis.checkbox("option xaxis 3")
+
+            st.write("Color/Group (Optional)")
+            cgroup = st.popover("Choose")
+            cgroupopt1 = cgroup.checkbox("option cgroup 1")
+            cgroupopt2 = cgroup.checkbox("option cgroup 2")
+            cgroupopt3 = cgroup.checkbox("option cgroup 3")
+
+            st.write("Aggregation")
+            aggreg = st.popover("Choose")
+            aggregopt1 = aggreg.checkbox("option aggreg 1")
+            aggregopt2 = aggreg.checkbox("option aggreg 2")
+            aggregopt3 = aggreg.checkbox("option aggreg 3")
+
+            st.space(size=10)
+
+            st.header("Filters")
+
+            st.write("Numeric Filter")
+            numFilt = st.popover("Choose")
+            numFiltopt1 = numFilt.checkbox("option numFilt 1")
+            numFiltopt2 = numFilt.checkbox("option numFilt 2")
+            numFiltopt3 = numFilt.checkbox("option numFilt 3")
+
+            st.write("Categorical filter")
+            catFilt = st.popover("Choose")
+            catFiltopt1 = catFilt.checkbox("option catFilt 1")
+            catFiltopt2 = catFilt.checkbox("option catFilt 2")
+            catFiltopt3 = catFilt.checkbox("option catFilt 3")
+
+            st.space(size=20)
+
+            genChart, resetFilt = st.columns([2,2])
+            with genChart:
+                st.button("Generate Chart")
+
+            with resetFilt:
+                st.button("Reset Filters")
+
+    with chartOutputColumn:
+        containerOutputVTab = st.container(border=True)
+        with containerOutputVTab:
+            st.header("Visualization Output")
+            
+            st.space(size=30)
+            st.header("HERE WILL BE VISUALIZED RESULTS")
 
 ##setting overview tab (will need change)
 with exportReportTab:
-    st.header("here will be export and report")
+    st.header("Export & Report")
+    st.write("Export your cleared dataset, transformation logs and reproducible workflow recipes")
+
+    st.space(size=35)
+
+    st.header("Final metrics")
+    finalrowsColumn, finalcolumnsColumn, transformationsColumn, validationViolationsColumn, lastChangeColumn = st.columns([2, 2, 2, 2, 2])
+
+    with finalrowsColumn:
+        st.header("Number of")
+        st.write("Final Rows")
+
+    with finalcolumnsColumn:
+        st.header("Number of")
+        st.write("Final Columns")
+
+    with transformationsColumn:
+        st.header("Number of")
+        st.write("Transformations Applied")
+
+    with validationViolationsColumn:
+        st.header("Number of")
+        st.write("Validations Violated")
+
+    with lastChangeColumn:
+        st.header("Date")
+        st.write("Last Change Date")
+
+    st.header("Export Options")
+
+    exportColumn, transformationReportColumn = st.columns([2,2])
+
+    with exportColumn:
+        exportContainer = st.container(border=True)
+        with exportContainer:
+            st.header("Export Dataset")
+            st.write("Download dataset in your preferred format")
+
+            dCSVColumn, dExcelColumn = st.columns([2,2])
+            with dCSVColumn:
+                st.button("Download CSV")
+
+            with dExcelColumn:
+                st.button("Download Excel")
+        
+    with transformationReportColumn:
+        transformationReportContainer = st.container(border=True)
+        with transformationReportContainer:
+            st.header("Transformation Report")
+            st.write("Download a detailed log of all operations applied, including parameters and timestamps")
+            st.button("Download report (.json)")
+    
+    exportWorkflowRecipeColumn, replayScriptColumn = st.columns([2,2])
+
+    with exportWorkflowRecipeColumn:
+        st.header("Export Workflow Recipe")
+        st.write("Download a machine readable JSON file representing the transformation pipeline")
+        st.button("Download Recipe (.json)")
+
+    with replayScriptColumn:
+        st.header("Replay Script")
+        st.write("Generate a pandas based Python script that reproduces the transformation steps")
+
+        genScriptColumn, downloadPYColumn = st.columns([2,2])
+        with genScriptColumn:
+            st.button("Generate script")
+        
+        with downloadPYColumn:
+            st.button("Download .py file")
+    
+    transformationContainer = st.container(border=True)
+    with transformationContainer:
+        st.header("Transformation Log")
+        st.write("Number of steps applied")
+
+        st.space(size=30)
+
+        st.header("HERE WILL BE LOADED LOGS")
+
+        undoLastStepColumn, resetAllTransformationsColumn = st.columns([2,2])
+        with undoLastStepColumn:
+            st.button("Undo Last Applied Step")
+    
+        with resetAllTransformationsColumn:
+            st.button("Reset All Transformations")
+
+    recipeJSONPreviewContainer = st.container(border=True)
+    with recipeJSONPreviewContainer:
+        st.header("Recipe JSON Preview")
